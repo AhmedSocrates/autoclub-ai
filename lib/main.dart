208bdc2ee437a887f35e0631d190007d3c6bf46f
AhmedSocrates <ahmedkinglion@gmail.com>
2026-04-27 08:19:21 +0800

    Sprint 1 folder structure is now tracable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'core/routing/app_router.dart'; // Import your new router
+import 'core/routing/app_router.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
