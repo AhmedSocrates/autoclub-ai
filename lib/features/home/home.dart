3ac5a184b24f838d1661592651cca2426c56e8df
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2026-04-29 23:58:39 +0800

    home screen changes

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:auto_club_ai/core/models/user.dart';
-import 'package:auto_club_ai/core/theme/app_text_styles.dart';
 import 'package:auto_club_ai/features/auth/bloc/auth_bloc.dart';
 import 'package:auto_club_ai/features/auth/bloc/auth_event.dart';
-import 'package:auto_club_ai/shared_widgets/alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
